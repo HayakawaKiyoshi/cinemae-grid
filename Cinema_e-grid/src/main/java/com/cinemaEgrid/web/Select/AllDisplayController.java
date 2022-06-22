c3424ff88fe5352b9d34494fc83871f7550650a8
iwai <iwai@EGRID-PC27.egrid.co.jp>
2022-06-22 17:40:45 +0900

    htmlの変更
    岩井 2022/06/22
    AllDisplayControllerの追加

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Select/AllDisplayController.java
@@ -0,0 +1,20 @@
+package com.cinemaEgrid.web.Select;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+// http://localhost:10000/admin/alldisplay
+
+@Controller
+@RequestMapping("/admin/alldisplay")
+public class AllDisplayController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	private ModelAndView index(ModelAndView mav) {
+		mav.setViewName("Admin/AllDisplay");
+		return mav;
+
+	}
+}
