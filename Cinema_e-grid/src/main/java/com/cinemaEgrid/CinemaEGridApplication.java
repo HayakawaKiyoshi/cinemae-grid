652f78f3286a1611286b5f082e826aaeb3b6eae9
matsuo <matsuo@EGRID-PC33.egrid.co.jp>
2022-06-22 14:49:16 +0900

    プロジェクトCinema e-grid
    松尾 6/22
    作成したプロジェクトです

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/CinemaEGridApplication.java
@@ -0,0 +1,13 @@
+package com.cinemaEgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaEGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaEGridApplication.class, args);
+	}
+
+}
---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/dao/DBManager.java
@@ -0,0 +1,43 @@
+package com.cinemaEgrid.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+	/**
+	* DBと接続する
+	*
+	* @return DBコネクション
+	* @throws ClassNotFoundException
+	* @throws SQLException
+	*/
+	public Connection getConn() throws ClassNotFoundException, SQLException {
+		// JDBCドライバクラスをJVMに登録
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		// DBへ接続
+		Connection conn = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:XE",
+				"xxxxxxx", "xxxxxx");
+		System.out.println("DBに接続しました");
+		return conn;
+	}
+
+	/**
+	* DB接続を切断する
+	*
+	* @param conn
+	* DBコネクション
+	*/
+	public void close(Connection conn) {
+		try {
+			// 切断処理
+			if (conn != null) {
+				conn.close();
+				System.out.println("切断しました");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
