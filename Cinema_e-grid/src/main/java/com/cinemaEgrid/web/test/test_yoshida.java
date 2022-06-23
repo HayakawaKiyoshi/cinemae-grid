5cadc3f11334df3f2ca03b72b4d689677b4ef502
yoshida <yoshida@EGRID-PC32.egrid.co.jp>
2022-06-23 10:33:55 +0900

    上映予定作成
    吉田公也　6/23
    
    上映予定のhtmlとテストコントローラ作成

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/test/test_yoshida.java
@@ -0,0 +1,26 @@
+package com.cinemaEgrid.web.test;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+//yoshida テスト用コントローラ
+@Controller
+public class test_yoshida {
+	@Autowired
+	HttpSession session;
+
+	//上映予定を表示するテストコントローラ
+
+	//ログイン画面に遷移するコントローラ
+	//http://localhost:10000/cinema/test
+	@RequestMapping(value = "/login/schedule", method = RequestMethod.GET)
+	private ModelAndView login( ModelAndView mav) {
+		mav.setViewName("User/Reserve/MovieSchedule");
+
+		return mav;
+	}
+}
