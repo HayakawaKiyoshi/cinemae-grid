8b4f8f0ee95ad339365d7fd94bdb15042cced8ed
iwai <iwai@EGRID-PC27.egrid.co.jp>
2022-06-23 13:31:08 +0900

    html/bean変更
    岩井 06/23
    storebeen/storeコントローラー変更

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/bean/Store.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/bean/Store.java
@@ -7,44 +7,51 @@ package com.cinemaEgrid.bean;
 */
 public class Store {
 
-	private int store_id;
+	private int store_no;
 	private String store_name;
 	private String store_location;
 	private int store_del_flg;
 
 	public Store(){}
 
-	public Store(int store_id, String store_name,
+	public Store(int store_no, String store_name,
 			String store_location, int store_del_flg){
-		this.store_id = store_id;
+		this.store_no = store_no;
 		this.store_name = store_name;
 		this.store_location = store_location;
 		this.store_del_flg = store_del_flg;
 	}
 
+	public int getStore_no() {
+		return store_no;
+	}
 
-	public int getStore_id() {
-		return store_id;
+	public void setStore_no(int store_no) {
+		this.store_no = store_no;
 	}
-	public void setStore_id(int store_id) {
-		this.store_id = store_id;
-	}
+
 	public String getStore_name() {
 		return store_name;
 	}
+
 	public void setStore_name(String store_name) {
 		this.store_name = store_name;
 	}
+
 	public String getStore_location() {
 		return store_location;
 	}
+
 	public void setStore_location(String store_location) {
 		this.store_location = store_location;
 	}
+
 	public int getStore_del_flg() {
 		return store_del_flg;
 	}
+
 	public void setStore_del_flg(int store_del_flg) {
 		this.store_del_flg = store_del_flg;
 	}
+
 }
