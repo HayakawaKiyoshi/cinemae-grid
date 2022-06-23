72aeb74f12156ee92c4ec064fa480db4943b749e
aishikawa <aishikawa@EG-TELEWORK07.egrid.co.jp>
2022-06-23 16:05:18 +0900

    ログインと会員登録
    石川 6/23
    ログインと会員登録を実装しました。
    レイアウトは途中です。
    確認、完了、cssも追加。

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Submit/SubmitController.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Submit/SubmitController.java
@@ -1,9 +1,19 @@
 package com.cinemaEgrid.web.Submit;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.cinemaEgrid.bean.User_TableBean;
+import com.cinemaEgrid.dao.User_TableDao;
+import com.cinemaEgrid.form.SubmitForm;
 
 /**
  * 会員登録
@@ -14,15 +24,48 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/login")
 public class SubmitController {
 
+	@Autowired
+	HttpSession session;
+
 	@RequestMapping("/userSubmit")
-	private String newUserSubmit() {
+	private String newUserSubmit(SubmitForm form) {
 		return "/Admin/Submit/newUserSubmit";
 	}
 
-	@RequestMapping(value="/userSubmit", method=RequestMethod.POST)
-	private ModelAndView newUserSubmitCheck(ModelAndView mav) {
-//		mav.setViewName("/Admin/Submit/newUserSubmit");
-		mav.setViewName("");
+	@RequestMapping(value="/userSubmit/check/{type}", method=RequestMethod.POST)
+	private ModelAndView newUserSubmitCheck(@PathVariable int type,
+			SubmitForm form, ModelAndView mav) {
+		if(type == 0) {
+			//新規会員登録の場合
+			session.setAttribute("type", type);
+		}
+		mav.setViewName("/Admin/Confirm/newUserSubmitConfirm");
+
 		return mav;
+	}
+
+	@RequestMapping(value="/userSubmit/completion", method=RequestMethod.POST)
+	private ModelAndView newUserSubmit(SubmitForm form, ModelAndView mav) {
+
+		//登録
+		ArrayList<User_TableBean> userList =
+				User_TableDao.submit(form.getId(), form.getName(), form.getMail(),
+				form.getPass(), form.getAuthority(), form.getDel_flg());
+
+		if(session.getAttribute("type").equals(0)) {
+			//新規会員登録の場合
+			String[] user = User_TableDao.login
+					(userList.get(0).getUser_id(), userList.get(0).getPassword());
+			session.setAttribute("user", user);
+			mav.addObject("msg", "会員登録");
+			mav.addObject("url", "/login/topPage");
+			mav.addObject("btn", "トップページへ");
+			mav.setViewName("/Admin/Done/memberDone");
+		} else {
+			//管理者が行う会員登録の場合
+
+		}
+		return mav;
+
 	}
 }
