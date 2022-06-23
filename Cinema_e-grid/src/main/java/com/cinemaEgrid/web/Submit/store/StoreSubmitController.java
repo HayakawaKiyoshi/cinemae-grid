4674ded46b0a3de052e550938cc11c6b31f6fe70
iwai <iwai@EGRID-PC27.egrid.co.jp>
2022-06-23 16:08:43 +0900

    名前変更/追加
    岩井 6/23
    名前変更/管理者店舗登録追加

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Submit/store/StoreSubmitController.java
@@ -0,0 +1,77 @@
+package com.cinemaEgrid.web.Submit.store;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cinemaEgrid.bean.Store;
+import com.cinemaEgrid.dao.StoreDao;
+
+//http://localhost:10000/admin/store/submit
+
+/**
+* 管理者/店舗情報登録
+* @author iwai
+*
+*/
+@Controller
+@RequestMapping("/admin/store/submit")
+@SessionAttributes("store")
+public class StoreSubmitController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	private ModelAndView index(@RequestParam("No") int id,
+			Store form, ModelAndView mav) {
+		List<Store> storelist = null;
+		try {
+			storelist = StoreDao.findStore(id);
+		} catch (SQLException e) {
+		}
+		//beanに情報コピー
+		BeanUtils.copyProperties(storelist.get(0), form);
+		mav.setViewName("Admin/Submit/store/storeSubmit");
+		return mav;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	private ModelAndView index2(
+			//@Validated
+			Store form,
+			//BindingResult result,
+			ModelAndView mav, Model model) {
+//		if (result.hasErrors()) {
+//			mav.setViewName("Admin/Update/store/storeUpdate");
+//		} else {
+			mav.setViewName("Admin/Confirm/store/storeCheck");
+//		}
+		return mav;
+	}
+
+	@RequestMapping(value = "/success", params = "back", method = RequestMethod.POST)
+	private ModelAndView index3(@Validated Store form, BindingResult result,
+			ModelAndView mav, Model model) {
+		mav.setViewName("Admin/Submit/store/storeSubmit");
+		return mav;
+	}
+
+	@RequestMapping(value = "/success", params = "exec", method = RequestMethod.POST)
+	private ModelAndView index4(Store form, ModelAndView mav) {
+		try {
+			StoreDao.registStore(form);
+		} catch (SQLException e) {
+		}
+		mav.setViewName("Admin/Done/submitSuccess");
+		return mav;
+	}
+
+}
