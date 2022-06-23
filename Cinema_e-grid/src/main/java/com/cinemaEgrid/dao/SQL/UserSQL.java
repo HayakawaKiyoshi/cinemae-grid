d805ebe3f35a7f765b046b656491265994ddea24
aishikawa <aishikawa@EG-TELEWORK07.egrid.co.jp>
2022-06-23 18:02:29 +0900

    再ブッシュ
    石川 6/23
    さっきと同じです

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/dao/SQL/UserSQL.java
@@ -0,0 +1,35 @@
+package com.cinemaEgrid.dao.SQL;
+
+public class UserSQL {
+
+	public static final String SELECT_ID_PASS =
+			"SELECT User_id, Authority, User_name, User_del_flg  "
+				+ "FROM User_table "
+				+ "WHERE User_id = ? AND Password = ?";
+
+	public static final String INSERT =
+			"INSERT INTO User_table "
+				+ "VALUES(?, ?, ?, ?, ?, ?)";
+
+	public static final String SELECT_ID =
+			"SELECT "
+				+ "User_id, "
+				+ "User_mail, "
+				+ "User_name, "
+				+ "Password, "
+				+ "Authority, "
+				+ "User_del_flg "
+				+ "FROM User_table "
+				+ "WHERE User_id = ?";
+
+	public static final String SELECT_ALL =
+			"SELECT "
+				+ "User_id, "
+				+ "User_mail, "
+				+ "User_name, "
+				+ "Password, "
+				+ "Authority, "
+				+ "User_del_flg "
+				+ "FROM User_table";
+
+}
