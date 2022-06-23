90d3ee786ccc36801f118365b255d8022f086501
matsuo <matsuo@EGRID-PC33.egrid.co.jp>
2022-06-23 11:36:46 +0900

    再プッシュ

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/bean/Movie.java
@@ -0,0 +1,86 @@
+package com.cinemaEgrid.bean;
+
+public class Movie {
+
+	private int movie_no;
+	private String movie_title;
+	private int genre1;
+	private int genre2;
+	private String time;
+	private int age_level;
+	private String release_day;
+	private String remarks;
+	private int movie_del_flg;
+
+	public int getMovie_no() {
+		return movie_no;
+	}
+
+	public void setMovie_no(int movie_no) {
+		this.movie_no = movie_no;
+	}
+
+	public String getMovie_title() {
+		return movie_title;
+	}
+
+	public void setMovie_title(String movie_title) {
+		this.movie_title = movie_title;
+	}
+
+	public int getGenre1() {
+		return genre1;
+	}
+
+	public void setGenre1(int genre1) {
+		this.genre1 = genre1;
+	}
+
+	public int getGenre2() {
+		return genre2;
+	}
+
+	public void setGenre2(int genre2) {
+		this.genre2 = genre2;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getAge_level() {
+		return age_level;
+	}
+
+	public void setAge_level(int age_level) {
+		this.age_level = age_level;
+	}
+
+	public String getRelease_day() {
+		return release_day;
+	}
+
+	public void setRelease_day(String release_day) {
+		this.release_day = release_day;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public int getMovie_del_flg() {
+		return movie_del_flg;
+	}
+
+	public void setMovie_del_flg(int movie_del_flg) {
+		this.movie_del_flg = movie_del_flg;
+	}
+}
