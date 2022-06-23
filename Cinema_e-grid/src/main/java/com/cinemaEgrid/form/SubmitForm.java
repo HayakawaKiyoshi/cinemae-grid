ab9d42a34b2240a33a6203ca03e0250c503176dc
aishikawa <aishikawa@EG-TELEWORK07.egrid.co.jp>
2022-06-23 16:06:03 +0900

    再コミット
    石川 6/23
    さっきと同じです。

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/form/SubmitForm.java
@@ -0,0 +1,50 @@
+package com.cinemaEgrid.form;
+
+public class SubmitForm {
+
+	private String id;
+	private String mail;
+	private String name;
+	private String pass;
+	private String authority;
+	private String del_flg;
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	public String getDel_flg() {
+		return del_flg;
+	}
+	public void setDel_flg(String del_flg) {
+		this.del_flg = del_flg;
+	}
+
+}
