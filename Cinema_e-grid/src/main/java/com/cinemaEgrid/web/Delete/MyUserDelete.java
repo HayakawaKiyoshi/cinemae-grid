ee5b6fb95cc7b4be421ebd03fc6fc61eb45be4b8
aishikawa <aishikawa@EG-TELEWORK07.egrid.co.jp>
2022-06-25 13:23:23 +0900

    退会
    石川 6/25
    退会完了

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Delete/MyUserDelete.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Delete/MyUserDelete.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cinemaEgrid.bean.User;
 import com.cinemaEgrid.dao.UserDao;
@@ -23,7 +24,15 @@ public class MyUserDelete {
 	@Autowired
 	HttpSession session;
 
+//	http://localhost:10000/login/delete
+
 	@RequestMapping("/delete")
+	private String myUserDeleteCheck() {
+		return "/Admin/Delete/user/myUserDelete";
+
+	}
+
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
 	private String myUserDelete() {
 //		String[] user = (String[]) session.getAttribute("user");
 //		ArrayList<User> userList = UserDao.search(user[0]);
@@ -31,7 +40,7 @@ public class MyUserDelete {
 		UserDao.delete(userList.get(0).getUser_id(), userList.get(0).getUser_mail(),
 				userList.get(0).getUser_name(), userList.get(0).getPassword(),
 				userList.get(0).getAuthority());
-		return "/Adnim/Done/myUserDeleteDone";
+		return "/Admin/Done/myUserDeleteDone";
 
 	}
 }
