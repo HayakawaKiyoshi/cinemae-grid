0892e1125ace360258e062af588392c5bb59dbd9
iwai <iwai@EGRID-PC27.egrid.co.jp>
2022-06-27 14:07:41 +0900

    会員削除フラグ機能
    岩井 6/27
    会員削除フラグ機能

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Delete/UserAdminDeleteController.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Delete/UserAdminDeleteController.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cinemaEgrid.bean.User;
 import com.cinemaEgrid.dao.UserAdminDao;
-
-// http://localhost:10000/admin/user/delete
 
 /**
 * 管理者/会員情報削除
@@ -22,7 +22,13 @@ import com.cinemaEgrid.dao.UserAdminDao;
 */
 @Controller
 @RequestMapping("/admin/user/delete")
+@SessionAttributes("user")
 public class UserAdminDeleteController {
+
+	@ModelAttribute("user")
+	public User setUpUser() {
+		return new User();
+	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	private ModelAndView index(@RequestParam("ID") String id,
@@ -41,13 +47,14 @@ public class UserAdminDeleteController {
 	@RequestMapping(value = "/success", method = RequestMethod.GET)
 	private ModelAndView index2(@RequestParam("ID") String id, User form,
 			ModelAndView mav, Model model) {
-		if (form.getUser_del_flg() == "1") {
+		if (form.getUser_del_flg().equals("1")) {
 			try {
 				UserAdminDao.deleteAdminUser(id);
 			} catch (SQLException e) {
 			}
 			mav.setViewName("Admin/Done/deleteDone");
 		} else {
+			mav.addObject("msg", "ユーザー削除フラグが機能しています");
 			mav.setViewName("redirect:/admin/alldisplay");
 		}
 		return mav;
