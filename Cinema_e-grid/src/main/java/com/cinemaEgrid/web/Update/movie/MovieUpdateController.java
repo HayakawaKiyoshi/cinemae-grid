3e00c5a1e21fa15921c2b4e280752b68ef42e72e
yoshida <yoshida@EGRID-PC32.egrid.co.jp>
2022-06-28 19:13:04 +0900

    再プッシュです
    吉田公也　6/28
    
    再プッシュです

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/movie/MovieUpdateController.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/movie/MovieUpdateController.java
@@ -1,5 +1,16 @@
 package com.cinemaEgrid.web.Update.movie;
 
-public class MovieUpdateController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MovieUpdateController {
+	//セッション準備
+	@Autowired
+	HttpSession session;
+	
+	
+	
 }
