9d0432079a3be2c24a56708ba360e1441ff3b2d4
yoshida <yoshida@EGRID-PC32.egrid.co.jp>
2022-06-28 15:07:29 +0900

    スケジュールのhtml変更
    吉田公也 6/28
    
    スケジュールのhtmlの編集ボタンを変更しました。

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Delete/movie/movieDeleteController.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Delete/movie/movieDeleteController.java
@@ -4,8 +4,12 @@ package com.cinemaEgrid.web.Delete.movie;
 
 import org.springframework.stereotype.Controller;
 
+
 @Controller
-public class movieDeleteController {
-//	@RequestMapping(value = "/admin/store/delete", method = RequestMethod.GET)
-//	private ModelAndView delete(@RequestParam(no) int no,)
+public class movieDeleteController {	//削除の際選択されたデータを表示するコントローラ
+	//@RequestMapping(value = "/admin/store/delete", method = RequestMethod.GET)
+//	private ModelAndView delete(@RequestParam("no") int no,ModelAndView mav) {
+//
+//	}
+
 }
