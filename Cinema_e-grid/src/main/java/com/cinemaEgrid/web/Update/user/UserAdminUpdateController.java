b58c953145348d17d2146fc1c73b062467b8af67
iwai <iwai@EGRID-PC27.egrid.co.jp>
2022-06-28 18:30:35 +0900

    結合
    iwai 6/28
    結合

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/user/UserAdminUpdateController.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/user/UserAdminUpdateController.java
@@ -1,17 +1,16 @@
 package com.cinemaEgrid.web.Update.user;
 import java.sql.SQLException;
-import java.util.List;
 
-import org.springframework.beans.BeanUtils;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cinemaEgrid.bean.User;
@@ -26,24 +25,15 @@ import com.cinemaEgrid.dao.UserAdminDao;
 */
 @Controller
 @RequestMapping("/admin/user/update")
-@SessionAttributes("user")
 public class UserAdminUpdateController {
-
-	@ModelAttribute("user")
-	public User setUpUser() {
-		return new User();
-	}
+	@Autowired
+	HttpSession session;
 
 	@RequestMapping(method = RequestMethod.GET)
 	private ModelAndView index(@RequestParam("ID") String id,
 			User form, ModelAndView mav) {
-		List<User> userlist = null;
-		try {
-			userlist = UserAdminDao.findOneUser(id);
-		} catch (SQLException e) {
-		}
-		//beanに情報コピー
-		BeanUtils.copyProperties(userlist.get(0), form);
+		User menber = UserAdminDao.findOneUser(id);
+		session.setAttribute("user", menber);
 		mav.setViewName("Admin/Update/user/userAdminUpdate");
 		return mav;
 	}
