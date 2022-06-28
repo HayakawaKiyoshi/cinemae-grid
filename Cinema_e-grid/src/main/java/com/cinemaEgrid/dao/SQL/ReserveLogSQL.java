1915d47a91200f63e8cfffa84857519729a15960
matsuo <matsuo@EGRID-PC33.egrid.co.jp>
2022-06-28 13:37:46 +0900

    CSS更新
    松尾 6/28
    Style.cssに内容を追加しました

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/dao/SQL/ReserveLogSQL.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/dao/SQL/ReserveLogSQL.java
@@ -3,6 +3,7 @@ package com.cinemaEgrid.dao.SQL;
 public class ReserveLogSQL {
 
 
+	//予約履歴取得SQL
 	public static final String RESERVE_LOG = " SELECT "
 			+ " TO_CHAR(schedule_date, 'YYYY-MM-DD') AS scheduleDate, "
 			+ " reserve_name, "
