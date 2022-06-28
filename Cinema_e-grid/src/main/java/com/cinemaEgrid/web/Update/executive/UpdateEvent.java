b8f2b171fdb735849dc3136a39d4c2bd91f5385d
aishikawa <aishikawa@EG-TELEWORK07.egrid.co.jp>
2022-06-28 15:39:40 +0900

    結合
    石川　6/28
    urlを記載、結合しました。
    管理者です。

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/executive/UpdateEvent.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/executive/UpdateEvent.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,11 +29,12 @@ public class UpdateEvent {
 	//イベントの番号をurlで送るように
 	//後にpostに変更するように
 
-	@RequestMapping("/eventUpdate/{type}")
-	private String eventUpdate(@PathVariable int type, EventForm form) {
+	@RequestMapping(value = "/eventUpdate/{type}", method = RequestMethod.POST)
+	private String eventUpdate(@PathVariable int type,
+			@ModelAttribute("eventId") String eventId, EventForm form) {
 		if (type == 0) {
 			//最初に入ってきた場合
-			ArrayList<EventExecutive> eventList = EventExecutiveDao.search("1");
+			ArrayList<EventExecutive> eventList = EventExecutiveDao.search(eventId);
 			form.setEventTitle(eventList.get(0).getUpdate_title());
 			form.setEventContent(eventList.get(0).getEvent_content());
 		}
