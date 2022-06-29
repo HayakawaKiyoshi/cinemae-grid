484ace0e78d4aa6a7a862513e1c6fe01215eb621
iwai <iwai@EGRID-PC27.egrid.co.jp>
2022-06-29 18:49:01 +0900

    結合
    iwai 6/28
    結合

---
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/dao/SQL/ReserveSQL.java
@@ -0,0 +1,37 @@
+package com.cinemaEgrid.dao.SQL;
+
+/**
+* 予約SQL
+*
+*/
+public class ReserveSQL {
+
+	//予約履歴取得SQL
+	public static final String RESERVE_LOG = " SELECT "
+			+ " TO_CHAR(schedule_date, 'YYYY-MM-DD') AS scheduleDate, "
+			+ " reserve_name, "
+			+ " schedule_no, "
+			+ " TO_CHAR(book_date, 'YYYY-MM-DD') AS bookDate, "
+			+ " book_code "
+			+ " FROM reserve_table WHERE user_id = ? ORDER BY book_date";
+
+
+	//予約キャンセル確認情報取得SQL
+	public static final String RESERVE_CANCEL = "SELECT\r\n"
+			+ "TO_CHAR(schedule_date, 'YYYY-MM-DD') AS scheduleDate,\r\n"
+			+ "reserve_name,\r\n"
+			+ "schedule_no,\r\n"
+			+ "TO_CHAR(book_date, 'YYYY-MM-DD') AS bookDate,\r\n"
+			+ "book_code\r\n"
+			+ "FROM reserve_table WHERE user_id = ? AND book_code = ?";
+
+
+	//予約キャンセル(データ削除)SQL
+	public static final String RESERVE_DELETE = "DELETE"
+			+ " FROM reserve_table WHERE user_id = ? AND book_code = ?";
+
+	//予約完了画面に予約情報を登録するsql
+		public static final String ADD_RESERVE =
+				"INSERT INTO reserve_table VALUES(?,?,?,?,?,?)";
+
+}
