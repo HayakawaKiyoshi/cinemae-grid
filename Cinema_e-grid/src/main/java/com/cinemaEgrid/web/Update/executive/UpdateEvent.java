1662a23e41611a7ee7aec48212327b30c13d754e
aishikawa <aishikawa@EG-TELEWORK07.egrid.co.jp>
2022-06-30 11:10:39 +0900

    イベント編集修正
    石川　6/30
    イベント編集の修正をしました。

--- Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/executive/UpdateEvent.java
+++ Cinema_e-grid/src/main/java/com/cinemaEgrid/web/Update/executive/UpdateEvent.java
@@ -2,6 +2,9 @@ package com.cinemaEgrid.web.Update.executive;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -24,12 +27,16 @@ import com.cinemaEgrid.form.EventForm;
 @RequestMapping("/login")
 public class UpdateEvent {
 
+	@Autowired
+	HttpSession session;
+
 	@RequestMapping(value = "/eventUpdate/{type}", method = RequestMethod.POST)
 	private String eventUpdate(@PathVariable int type,
 			@ModelAttribute("eventId") String eventId, EventForm form) {
 		if (type == 0) {
 			//最初に入ってきた場合
 			ArrayList<EventExecutive> eventList = EventDao.search(eventId);
+			session.setAttribute("eventId", eventId);
 			form.setEventTitle(eventList.get(0).getUpdate_title());
 			form.setEventContent(eventList.get(0).getEvent_content());
 		}
@@ -47,9 +54,8 @@ public class UpdateEvent {
 
 	@RequestMapping(value = "/eventUpdate/done", method = RequestMethod.POST)
 	private ModelAndView eventUpdateDone(EventForm form, ModelAndView mav) {
-		EventDao.update("1", form.getEventTitle(),
+		EventDao.update((String) session.getAttribute("eventId"), form.getEventTitle(),
 				form.getEventContent(), form.getUpdateDate());
-		System.out.println();
 		mav.addObject("title", "イベント更新完了");
 		mav.addObject("msg", "イベントの更新");
 		mav.addObject("url", "/toppage");
